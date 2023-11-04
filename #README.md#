fe877c41a16f173979fbf6c7a19add0bc1a56bb2
anis <anissafia110@gmail.com>
2023-11-04 05:04:09 -0700

    add all files

---
+++ #README.md#
@@ -0,0 +1,2 @@
+Project Name
+0x16. C - Simple Shell
---
+++ _initAppData.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ * _initData - init data structure
+ * @prmArgv: pointer to pointer to
+ *
+ * Return: data structure
+ */
+appData_t *_initData(char **prmArgv)
+{
+  appData_t *appData;
+
+  appData = malloc(sizeof(appData_t) * 1);
+
+  if (appData == NULL)
+    return (NULL);
+
+  appData->arguments = NULL;
+  appData->buffer = NULL;
+  appData->commandName = NULL;
+  appData->commandList = NULL;
+  appData->history = NULL;
+  appData->env = NULL;
+  appData->programName = (prmArgv[0] != NULL) ? prmArgv[0] : NULL;
+  _initEnvData(appData);
+
+  return (appData);
+}
